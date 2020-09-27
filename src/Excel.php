92e3ff82bec1b53ab6f021678bf0eb83c77a829b
alanchen365 <alanchen365@gmail.com>
2020-09-27 16:25:34 +0800

    更新获取表格

--- src/Excel.php
+++ src/Excel.php
@@ -4,22 +4,21 @@ namespace Es3\Utility;
 
 class Excel
 {
-    static function xlswriterMapping(string $filePath, array $mapping, int $startRow = 2, string $sheetName = null, array $xlswriterConfig = []): array
+    static function xlswriterMapping(string $filePath, array $mapping, int $skipRows = null, string $sheetName = null, array $xlswriterConfig = []): array
     {
-        // 读取测试文件
+        // 打开文件
         $xlswriterConfig['path'] = '/';
         $excel = new \Vtiful\Kernel\Excel($xlswriterConfig);
         $sheetList = $excel->openFile($filePath)->sheetList();
 
+        /** 获取 sheet */
         $sheetName = superEmpty($sheetName) ? current((array)$sheetList) : $sheetName;
         $excel->openFile($filePath)->openSheet($sheetName, \Vtiful\Kernel\Excel::SKIP_EMPTY_ROW);
 
-        /**  开始行数对外界 */
-        $startRow = $startRow - 1;
-        $startRow = $startRow > 1 ? 0 : $startRow;
-
+        /** 获取结果 */
         $results = [];
-        for ($row = $startRow; ($rowData = $excel->nextRow()) !== NULL; $row++) {
+        $skipRows ? $excel->setSkipRows($skipRows) : null;
+        for ($row = 1 + intval($skipRows); ($rowData = $excel->nextRow()) !== NULL; $row++) {
 
             /** 如果正常是空的 就过滤掉 */
             if (empty(array_filter($rowData))) {
