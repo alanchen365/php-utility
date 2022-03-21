5b71c53049d527003b5861330f3d9ed3c9524558
alanchen365 <alanchen365@gmail.com>
2022-03-21 18:29:14 +0800

    增加邮箱合法验证

---
+++ src/Mail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Util;
+
+class Mail
+{
+    /**
+     * 邮箱合法性校验
+     * @param string $mail
+     * @return bool
+     */
+    public static function isLegal(string $mail): bool
+    {
+        $rule = "/^([a-z0-9+_]|\\-|\\.)+@(([a-z0-9_]|\\-)+\\.)+[a-z]{2,6}\$/i";
+        if (strpos($mail, '@') !== false && strpos($mail, '.') === false) {
+            return false;
+        }
+
+        return preg_match($rule, $mail);
+    }
+}
